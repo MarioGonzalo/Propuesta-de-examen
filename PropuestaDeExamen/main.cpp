68531cf09b78bef46e9d3eaf4134a58150158973
MarioGonzalo <146821965+MarioGonzalo@users.noreply.github.com>
2023-11-07 11:33:18 +0100

    Intento 2.1

--- PropuestaDeExamen/main.cpp
+++ PropuestaDeExamen/main.cpp
@@ -2,13 +2,14 @@
 #include <cmath>
 
 int main() {
-    int a = 0;
-    int b = 0;
-    int c;
-    bool triplePitagorico = false;
+    //Se declaran los números con los que vamos a tarabajar
+    int a = 0; // a (cateto 1)
+    int b = 0; // b (cateto 2)
+    int c; //c (hipotenusa)
     for (int i = 0; i < 20; ++i) {
-        a++;
-        for (int j = 0; j < 1; ++j) {
+        b=0;
+        a ++;
+        for (int j = 0; j < 20; ++j) {
             b++;
             std::string pot = std::to_string(sqrt(pow(a,2) + pow(b,2)));
             if (pot.find(".") == false ) {
